8b759bb2cc904027c3d8f7d047b8a5d2ba8bebc1
humdrum <jjrajani@gmail.com>
2016-06-22 13:07:48 -0400

    added carousel proect

---
+++ hardMode/js/imgs.js
@@ -0,0 +1,17 @@
+var carouselImages = [
+  {
+    image_url: 'https://interfacelift.com/wallpaper/7yz4ma1/03965_sunsetinthecascades_1280x720.jpg',
+    title: 'Sunset in the Cascades',
+    photographer_id: 'smpoutdoors'
+  },
+  {
+    image_url: 'https://interfacelift.com/wallpaper/7yz4ma1/03958_seljalandsfossiceland_1280x720.jpg',
+    title: 'Seljalandsfoss, Iceland',
+    photographer_id: 'Robin Kamp'
+  },
+  {
+    image_url: 'https://interfacelift.com/wallpaper/7yz4ma1/03950_godsrays_1280x720.jpg',
+    title: 'Gods Rays',
+    photographer_id: 'illuminati'
+  }
+];
